cd9fef2d72986f8de9f79fc2f7444e624c387330
Joel Panisello <joel.panisello@estudiants.urv.cat>
2021-03-08 18:38:46 +0100

    redis and workers

--- Prac1/redisTest.py
+++ Prac1/redisTest.py
@@ -1,11 +1,25 @@
 import redis
-    
-def main():
-    r = redis.Redis()
-    r.mset({'Croatia': 'Zagreb'})
-    print(r.get('Croatia'))
+import time
+import json
+import requests
+def send_operation_to_redis_queue(conn, operation, file_URL):
+   data = {
+        'operation': operation,
+        'file_URL': file_URL,
+        'time': time.time()
+   }
+   conn.rpush('queue:jobs', json.dumps(data))
 
-if __name__ == '__main__':
-    
-    import sys
-    sys.exit(int(main() or 0))
+def get_redis_job_queue(conn):
+    packed = conn.blpop(['queue:jobs'], 0)
+    to_send = json.loads(packed[1])
+    print(packed[2])
+    print(to_send)
+
+def do_request(URL):
+    return requests.get(URL).text
+
+
+print(do_request("http://localhost:8000/client.py"))
+
+#blpop rpush
--- Prac1/workers.py
+++ Prac1/workers.py
@@ -1,10 +1,12 @@
 from multiprocessing import Process
-
+import time
 WORKERS = {}
 WORKER_ID = 0
 
-def start_worker():
-
+def start_worker(id):
+    time.sleep(2)
+    print('Echo',id)
+    time.sleep(30)
 
 def create_worker():
     global WORKERS
@@ -15,3 +17,18 @@ def create_worker():
     WORKERS[WORKER_ID] = proc
 
     WORKER_ID += 1
+
+def delete_worker(id):
+    global WORKERS
+    WORKERS[id].terminate()
+    del WORKERS[id]
+
+
+
+create_worker()
+create_worker()
+print(WORKERS)
+create_worker()
+print(WORKERS)
+delete_worker(1)
+print(WORKERS)
