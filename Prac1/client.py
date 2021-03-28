bc035c31a8b358d2acb8d69705498802654ad263
Joel Panisello <joel.panisello@estudiants.urv.cat>
2021-03-28 17:39:24 +0200

    more updates

--- Prac1/client.py
+++ Prac1/client.py
@@ -8,6 +8,8 @@ stub = calculator_pb2_grpc.CalculatorStub(channel)
 
 operation = calculator_pb2.Operation(operation="wordCount", url=["http://localhost:8000/fitxer1.txt", "http://localhost:8000/fitxer2.txt"])
 response1 = stub.create_worker(calculator_pb2.Empty())
+response1 = stub.create_worker(calculator_pb2.Empty())
+
 response2 = stub.job_worker(operation)
 
 print(response1.value)
--- Prac1/workers.py
+++ Prac1/workers.py
@@ -2,25 +2,39 @@ from multiprocessing import Process
 import redisOperations
 import time
 import calculator
+import json
+from ast import literal_eval
 WORKERS = {}
 WORKER_ID = 0
 
 def start_worker(id):
     while True:
         job = redisOperations.get_redis_job_queue(redisOperations.QUEUE_JOBS)
-        print(len(job['file_URL']))
-        print("Operacion recibida!!!!!!")
-        redisOperations.send_operation_to_redis_queue(len(job['file_URL']), '', job['id_queue_result'], '')
-        
-        #text = calculator.do_request(job['file_URL'])
-        #if job['operation'] == 'countingWords':
-        #    result = calculator.countingWords(text) 
-        #elif job['operation'] == 'wordCount':
-        #    result = calculator.wordCount(text)
-        #redisOperations.send_operation_to_redis_queue(result, '', job['id_queue_result'], '')
-
-        print("Resultado enviado!")
-
+        if (len(job['file_URL']) == 1):
+            text = calculator.do_request(job['file_URL'][0])
+            if job['operation'] == 'countingWords':
+                result = calculator.countingWords(text) 
+            elif job['operation'] == 'wordCount':
+                result = calculator.wordCount(text)
+        elif (len(job['file_URL']) > 1):
+            id_worker = 'queue:worker'+str(id)
+            for i in range(len(job['file_URL'])):
+                redisOperations.send_operation_to_redis_queue(job['operation'], [job['file_URL'][i]], redisOperations.QUEUE_JOBS, id_worker)
+            if job['operation'] == 'countingWords':
+                result = 0
+                for i in range(len(job['file_URL'])):
+                    result += redisOperations.get_redis_job_queue(id_worker)['operation']
+            elif job['operation'] == 'wordCount':
+                result = {}
+                for i in range(len(job['file_URL'])):
+                    aux = literal_eval(redisOperations.get_redis_job_queue(id_worker)['operation'])
+                    for key in aux:
+                        if key in result:
+                            result[key] += aux[key]
+                        else:
+                            result[key] = aux[key]
+        redisOperations.send_operation_to_redis_queue(result, '', job['id_queue_result'], '')
+    
 
 # create_worker: create a worker
 def create_worker():
